b303ac328d2b61d574aefa6feb11c1ec6b40e4bc
ssun <pros5018@nate.com>
2023-07-22 15:25:49 +0900

    20230722 5st class'

--- designhotels/js/script.js
+++ designhotels/js/script.js
@@ -31,14 +31,14 @@ $('#mainVideo').get(0).autoplay=true;
 $('#mainVideo').get(0).loop=0;
 $('#mainVideo').get(0).muted=true;
 
-$('.video').css({width:'100%', height: winH});
+$('.video').css({width:'100%', height:winH});
 
-if( winH > vidH ) {
+//if( winH > vidH ) {
     $('#mainVideo').css({width:'auto', height:winH})
-}
-if( winW > vidW ) {
+//}
+//if( winW > vidW ) {
     $('#mainVideo').css({width:winW, height:'auto'})
-}
+//}
 
 $(window).resize(videoResizeFn);
 
@@ -66,13 +66,74 @@ $('.pauseIcon').on({click:function(){
         videoPlay = 'off';
         $('#mainVideo').get(0).pause();
         $(this).find('i').attr('class', 'fas fa-play');
-    } else {
-        $('#mainVideo').get(0).play();
+    } else {        
         videoPlay = 'on';
+        $('#mainVideo').get(0).play();
         $(this).find('i').attr('class', 'fas fa-pause');
     }
 }});
 
 $(document).keypress(function(e) {
+    
+    if( e.keyCode === 32 && videoPlay === 'on' ) {
+        e.preventDefault();
+        videoPlay = 'off';
+        $('#mainVideo').get(0).pause();
+        $('.pauseIcon').find('i').attr('class', 'fas fa-play');
 
-})
+    } else if( e.keyCode === 32 && videoPlay === 'off' ) {
+        videoPlay = 'on';
+        $('#mainVideo').get(0).play();
+        $('.pauseIcon').find('i').attr('class', 'fas fa-pause');
+    }
+
+    if( e.keyCode === 13 && soundMuted === 'on' ) {
+        e.preventDefault();
+        soundMuted = 'off';
+        $('#mainVideo').get(0).muted = true;
+        $('.mutedIcon').find('i').attr('class', 'fas fa-volume-mute');
+
+    } else if( e.keyCode === 13 && soundMuted === 'off' ) {
+        soundMuted = 'on';
+        $('#mainVideo').get(0).muted = false;
+        $('.mutedIcon').find('i').attr('class', 'fas fa-volume-up');
+    }
+
+});
+
+$('.mutedIcon').on({click:function(){
+    if( soundMuted === 'off' ) {
+        soundMuted = 'on';
+        $('#mainVideo').get(0).muted = false;
+        $(this).find('i').attr('class', 'fas fa-volume-up');
+    } else {
+        soundMuted = 'off';
+        $('#mainVideo').get(0).muted = true;
+        $(this).find('i').attr('class', 'fas fa-volume-mute');
+    }
+}});
+
+
+let setId = setInterval(videoTimeCountFn, 1000);
+function videoTimeCountFn() {
+    //console.log('비디오 진행 시간: ' + $('#mainVideo').get(0).currentTime);
+    /* 
+    console.log('비디오 진행 시간: ' + $('#mainVideo').get(0).duration);
+    console.log('비디오 정지 여부: ' + $('#mainVideo').get(0).ended); 
+    */
+   if( $('#mainVideo').get(0).ended ) {
+       $('.m-again').show();
+       videoPlay='off';
+       $('.pauseIcon').find('i').attr('class', 'fas fa-play');
+       clearInterval(setId);
+   }
+}
+
+$('.m-again').on({click:function(){
+    videoPlay='on';
+    $('#mainVideo').get(0).play();
+    $('.pauseIcon').find('i').attr('class', 'fas fa-pause');
+    $(this).hide();
+
+    setId = setInterval(videoTimeCountFn, 1000);
+}});
