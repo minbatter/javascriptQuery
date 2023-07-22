3043f63ab3c97baa3c491db0ca1c288cc84b3c2e
ssun <pros5018@nate.com>
2023-07-22 12:42:12 +0900

    20230722 3st class

--- designhotels/js/script.js
+++ designhotels/js/script.js
@@ -1,7 +1,13 @@
 const appbar=$('.app-bar-wrap .appbarBt');
 const mobileMenu=$('.mobile-menu');
+const appbarCloseBt=$('.appbarCloseBt');
 
 appbar.on('click', function(){
     mobileMenu.stop().animate({left:0}, 500);
     $('.header-mobile-menu').stop().animate({left:'100%'}, 500);
-})
+});
+
+appbarCloseBt.on('click', function(){
+    mobileMenu.stop().animate({left:'-100%'}, 500);
+    $('.header-mobile-menu').stop().animate({left:0}, 500);
+});
